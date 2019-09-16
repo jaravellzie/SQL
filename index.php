7af5b0880de8b3805bc0edc0803a3cbe903394a8
jaravellzie <53119278+jaravellzie@users.noreply.github.com>
2019-09-16 11:16:19 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html>
+<head>
+	<title>SQL codes</title>
+	<link rel="stylesheet" href="JQueryUI1.11.4/jquery-ui.min.css">
+	<script type="text/javascript" src="JQueryUI1.11.4/jquery-2.1.4.min.js"></script>
+	<script type="text/javascript" src="JQueryUI1.11.4/jquery-ui.min.js"></script>
+	<script>
+		$(function(){
+			$("#accordion").accordion();
+		});
+	</script>
+</head>
+<body>
+
+<div id="accordion">
+	
+</div>
+
+</body>
+</html>
